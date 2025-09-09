0720bd20150d98e98bf85246aefcbae8c0f895b8
miggy88 <cunananluismiguel11@gmail.com>
2025-09-09 21:03:06 +0800

    Step 9

--- LabExer03/FrmRegistration.cs
+++ LabExer03/FrmRegistration.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -22,8 +23,49 @@ namespace LabExer03
 
         public FrmRegistration()
         {
+
+
+
             InitializeComponent();
         }
+            public long StudentNumber(string studNum)
+        {
+
+            _StudentNo = long.Parse(studNum);
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+
+            return _Age;
+        }
+        
 
         private void OrganizationProfile_Load(object sender, EventArgs e)
         {
@@ -44,7 +86,7 @@ namespace LabExer03
 
                 for (int i = 0; i < 6; i++)
             {
-                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());  
             }
 
         }
