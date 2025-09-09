d697f5aae1c49a67b03f1e9706e86ce57733c65b
miggy88 <cunananluismiguel11@gmail.com>
2025-09-09 12:30:48 +0800

    Step 4 done

---
+++ LabExer03/StudentInformationClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExer03
+{
+    class StudentInformationClass
+    {
+        public int SetStudentNo = 0;
+        public int SetContactNo = 0;
+        public string SetProgram = "";
+        public string SetGender= "";
+        public string SetBirthday = "";
+        public string SetFullName = "";
+
+
+
+
+    }
+}
